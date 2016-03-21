9dcf2fb9cbcbd5749566afa14549a024a0d687c8
Troy <pellet325@gmail.com>
2016-03-21 13:14:17 +0000

    added README

---
+++ app/README.md
@@ -0,0 +1,21 @@
+# Gakktu Mobile App
+
+Gakktu Mobile App is built for Android.
+
+# Installation
+
+Clone the project from here:
+https://github.com/tap4/GakktuMobile
+
+# Install requirements
+
+npm install -g maven
+npm install -g gradle
+
+# To run the app:
+
+Open project in Android Studio
+Open AVD (Android Virtual Device Manager) and create a Virtual Device (we used Nexus 5 API 23)
+Build your virtual device and "Run App"
+
+
--- app/src/main/java/com/example/troyporter/troysfirstapp/ProfileActivity.java
+++ app/src/main/java/com/example/troyporter/troysfirstapp/ProfileActivity.java
@@ -1,8 +1,10 @@
 package com.example.troyporter.troysfirstapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,6 +16,14 @@ import android.util.Log;
 public class ProfileActivity extends AppCompatActivity {
 
     private TextView mUserPreferenceChildrenTextView;
+    private TextView mUserPreferenceEmployedTextView;
+    private TextView mUserPreferenceDisabledTextView;
+    private TextView mUserPreferenceCitizenshipTextView;
+
+    //Array of Strings describing UserInterest Level, to display in the TextView
+    private String[] interestString= new String[]{
+            "Low", "Neutral", "High"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,15 +34,88 @@ public class ProfileActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mUserPreferenceChildrenTextView = (TextView) findViewById(R.id.user_pref_children_view);
+        mUserPreferenceEmployedTextView = (TextView) findViewById(R.id.user_pref_employed_view);
+        mUserPreferenceDisabledTextView = (TextView) findViewById(R.id.user_pref_disabled_view);
+        mUserPreferenceCitizenshipTextView = (TextView) findViewById(R.id.user_pref_citizenship_view);
 
         //----Get the User's Profile Preferences from SharedPreferences File and display on screen---//
         SharedPreferences sharedPref = getSharedPreferences(
                 getString(R.string.userProfilePreferences), Context.MODE_APPEND);
         int childPrefValue =  sharedPref.getInt("children", 1);
-        mUserPreferenceChildrenTextView.setText(String.valueOf(childPrefValue));
+        int employedPrefValue =  sharedPref.getInt("employed", 1);
+        int disabledPrefValue =  sharedPref.getInt("disabled", 1);
+        int citizenshipPrefValue =  sharedPref.getInt("citizenship", 1);
+
+        String childPrefString = interestString[childPrefValue];
+        String employedPrefString = interestString[employedPrefValue];
+        String disabledPrefString = interestString[disabledPrefValue];
+        String citizenshipPrefString = interestString[citizenshipPrefValue];
+
+        mUserPreferenceChildrenTextView.setText(childPrefString);
+        mUserPreferenceEmployedTextView.setText(employedPrefString);
+        mUserPreferenceDisabledTextView.setText(disabledPrefString);
+        mUserPreferenceCitizenshipTextView.setText(citizenshipPrefString);
+
+
+        //Set Text Color of ChildPreference
+        if (childPrefString=="Low")
+        {
+            mUserPreferenceChildrenTextView.setTextColor(Color.RED);
+        }
+        if (childPrefString=="Neutral")
+        {
+            mUserPreferenceChildrenTextView.setTextColor(Color.BLUE);
+        }
+        if (childPrefString=="High")
+        {
+            mUserPreferenceChildrenTextView.setTextColor(Color.GREEN);
+        }
+
+        //Set Text Color of Employed Preference
+        if (employedPrefString=="Low")
+        {
+            mUserPreferenceEmployedTextView.setTextColor(Color.RED);
+        }
+        if (employedPrefString=="Neutral")
+        {
+            mUserPreferenceEmployedTextView.setTextColor(Color.BLUE);
+        }
+        if (employedPrefString=="High")
+        {
+            mUserPreferenceEmployedTextView.setTextColor(Color.GREEN);
+        }
+
+        //Set Text Color of Disabled Preference
+        if (disabledPrefString=="Low")
+        {
+            mUserPreferenceDisabledTextView.setTextColor(Color.RED);
+        }
+        if (disabledPrefString=="Neutral")
+        {
+            mUserPreferenceDisabledTextView.setTextColor(Color.BLUE);
+        }
+        if (disabledPrefString=="High")
+        {
+            mUserPreferenceDisabledTextView.setTextColor(Color.GREEN);
+        }
+
+        //Set Text Color of Citizenship Preference
+        if (citizenshipPrefString=="Low")
+        {
+            mUserPreferenceCitizenshipTextView.setTextColor(Color.RED);
+        }
+        if (citizenshipPrefString=="Neutral")
+        {
+            mUserPreferenceCitizenshipTextView.setTextColor(Color.BLUE);
+        }
+        if (citizenshipPrefString=="High")
+        {
+            mUserPreferenceCitizenshipTextView.setTextColor(Color.GREEN);
+        }
+
         //-------------------------------------------------------------------------------------//
 
-        //Android put this here.  We can delete later if we dont want it.
+        //Android put this here.  We can delete later if we don't want it.
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
